99120c8de82b7bff9d357b3a01d7efd9c3a046eb
good0neighbour <notyeas@gmail.com>
2022-07-24 21:18:08 +0900

    FirstCreation
    Nothing here.

---
+++ GaussianElimination/GaussianElimination/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace GaussianElimination
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
